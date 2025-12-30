810f888872831e3ead4cfbc845acb6cc0c7593e2
github_action <ci_email@yolobuyer.com>
2025-12-30 21:15:05 +0800

    opt

--- src/config/faqs.ts
+++ src/config/faqs.ts
@@ -2,18 +2,27 @@ import type { FAQ } from "@/types/app";
 
 export const faqs: FAQ[] = [
 	{
-		question: "如何通过 XCode 安装已签名的 IPA 文件？",
-		href:"xcode_install",
-		answer: "Answer to question 1 goes here.",
-	},
-	{
-		question: "Question 2?",
-		href:"xcode_install",
+		question: "什么是 IOS 侧载",
+		href: "how_to_install",
+		answer: "Answer to question 2 goes here.",
+	}, {
+		question: "SideLoadly 使用",
+		href: "side_loadly_manual",
 		answer: "Answer to question 2 goes here.",
 	},
 	{
+		question: "如何安装",
+		href: "how_to_install",
+		answer: "Answer to question 2 goes here.",
+	},
+	{
+		question: "如何通过 XCode 安装已签名的 IPA 文件？",
+		href: "xcode_install",
+		answer: "Answer to question 1 goes here.",
+	},
+	{
 		question: "Question 3?",
-		href:"xcode_install",
+		href: "xcode_install",
 		answer: "Answer to question 3 goes here.",
 	},
 ];
