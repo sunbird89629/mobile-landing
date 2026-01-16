989c639e2dce8e26611c90768c6059c9ab51a156
github_action <ci_email@yolobuyer.com>
2026-01-16 11:14:35 +0800

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: "https://mt.aaaabb.cc",
+  integrations: [tailwind(), react(), sitemap()],
   output: "server",
   adapter: cloudflare({
     imageService: "compile",
